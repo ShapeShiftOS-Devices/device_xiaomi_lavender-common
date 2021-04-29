e643c21be81ea3f5320dc04d689ad5bbfd2a84f8
Omar Hamad <etahamad@icloud.com>
2021-04-29 06:17:15 +0200

    sdm60-common: ENABLE_AB -> AB_OTA_UPDATER
    ENABLE_AB is useless, AB_OTA_UPDATER can do the same job.

--- sdm660.mk
+++ sdm660.mk
@@ -57,7 +57,7 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # A/B
-ifeq ($(ENABLE_AB), true)
+ifeq ($(AB_OTA_UPDATER), true)
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
